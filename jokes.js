75653d08742bef1eeafcf5b4157f01b574e3f048
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-04 18:31:01 -0300

    Adicionado pacote Axios

--- jokes.js
+++ jokes.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 // Common Javascript:
 
 // module.exports = {
@@ -14,14 +16,27 @@
 
 // ES6 Javascript:
 
+// export const jokes = {
+//     getOne: function() {
+//         return new Promise((resolve, reject) => {
+//             fetch('https://api.chucknorris.io/jokes/random')
+//                 .then(result => result.json())
+//                 .then(data => {
+//                     resolve(data.value);
+//                 })
+//         })
+//     }
+// }
+
+// Using Axios 
+
 export const jokes = {
-    getOne: function() {
+    getOne: function () {
         return new Promise((resolve, reject) => {
-            fetch('https://api.chucknorris.io/jokes/random')
-                .then(result => result.json())
-                .then(data => {
-                    resolve(data.value);
-                })
+            axios.get('https://api.chucknorris.io/jokes/random')
+                .then(result => {
+                    resolve(result.data.value);
+                });
         })
     }
 }
